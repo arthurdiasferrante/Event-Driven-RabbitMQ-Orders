fc7c9e3d3308acdd15a8b9b3e7123373846665fa
Arthur <arthurdiasferrante@gmail.com>
2026-04-01 13:19:43 -0300

    feat: add DELETE and UPDATE endpoints completing the orders CRUD

--- pizzaria/src/main/java/com/pizzaria/controller/OrderController.java
+++ pizzaria/src/main/java/com/pizzaria/controller/OrderController.java
@@ -3,11 +3,13 @@ package com.pizzaria.controller;
 
 import com.pizzaria.model.Order;
 import com.pizzaria.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Reader;
 import java.util.List;
 
 @RequestMapping("/orders")
@@ -38,6 +40,30 @@ public class OrderController {
         }
         Order order = orderEntity.get();
         return ResponseEntity.ok(order);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@RequestBody Order newOrder, @PathVariable Long id) {
+        var orderEntity = orderRepository.findById(id);
+        if (orderEntity.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Order updatedOrder = orderEntity.get();
+        updatedOrder.setPizzas(newOrder.getPizzas());
+        orderRepository.save(updatedOrder);
+
+        return ResponseEntity.ok(updatedOrder);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        var orderEntity = orderRepository.findById(id);
+        if (orderEntity.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        orderRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
