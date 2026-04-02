df9849f8bd555667e2bf81f0969eac6e84ff008e
Arthur <arthurdiasferrante@gmail.com>
2026-04-02 20:49:00 -0300

    refactor: ordercontroller general refactor

--- pizzaria/src/main/java/com/pizzaria/controller/OrderController.java
+++ pizzaria/src/main/java/com/pizzaria/controller/OrderController.java
@@ -1,69 +1,66 @@
-package com.pizzaria.controller;
+    package com.pizzaria.controller;
 
 
-import com.pizzaria.model.Order;
-import com.pizzaria.repository.OrderRepository;
-import org.aspectj.weaver.ast.Or;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import com.pizzaria.model.Order;
+    import com.pizzaria.repository.OrderRepository;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import java.io.Reader;
-import java.util.List;
+    import java.util.List;
 
-@RequestMapping("/orders")
-@RestController
-public class OrderController {
+    @RequestMapping("/orders")
+    @RestController
+    public class OrderController {
 
-    private final OrderRepository orderRepository;
+        private final OrderRepository orderRepository;
 
-    public OrderController(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
-
-    @PostMapping
-    public Order createOrder(@RequestBody Order order) {
-        return orderRepository.save(order);
-    }
-
-    @GetMapping
-    public List<Order> getOrders() {
-        return orderRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
-        var orderEntity = orderRepository.findById(id);
-        if (orderEntity.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        Order order = orderEntity.get();
-        return ResponseEntity.ok(order);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Order> updateOrder(@RequestBody Order newOrder, @PathVariable Long id) {
-        var orderEntity = orderRepository.findById(id);
-        if (orderEntity.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        Order updatedOrder = orderEntity.get();
-        updatedOrder.setPizzas(newOrder.getPizzas());
-        orderRepository.save(updatedOrder);
-
-        return ResponseEntity.ok(updatedOrder);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
-        var orderEntity = orderRepository.findById(id);
-        if (orderEntity.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        public OrderController(OrderRepository orderRepository) {
+            this.orderRepository = orderRepository;
         }
 
-        orderRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
+        @PostMapping
+        public Order createOrder(@RequestBody Order order) {
+            return orderRepository.save(order);
+        }
 
-}
+        @GetMapping
+        public List<Order> getOrders() {
+            return orderRepository.findAll();
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+            var orderEntity = orderRepository.findById(id);
+            if (orderEntity.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+            Order order = orderEntity.get();
+            return ResponseEntity.ok(order);
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<Order> updateOrder(@RequestBody Order newOrder, @PathVariable Long id) {
+            var orderEntity = orderRepository.findById(id);
+            if (orderEntity.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+            Order updatedOrder = orderEntity.get();
+            updatedOrder.setPizzas(newOrder.getPizzas());
+            orderRepository.save(updatedOrder);
+
+            return ResponseEntity.ok(updatedOrder);
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+            var orderEntity = orderRepository.findById(id);
+            if (orderEntity.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+
+            orderRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+
+    }
